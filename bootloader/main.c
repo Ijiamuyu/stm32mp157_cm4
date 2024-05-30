e3bfd5fc807bac4fc742d7a449191e6f3ff815d7
Jiamuyu <Yjiamuyu@outlook.com>
2024-05-30 22:44:48 +0800

    support build & openocd

--- bootloader/main.c
+++ bootloader/main.c
@@ -1,11 +1,13 @@
-#include <stdio.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 
-int main(void)
-{
 
-    while (1) {
-    }
-    return 0;
+int main(void) {
+  volatile uint32_t addr = 0x10020000;
+  *(uint32_t *)addr = 0x20240530;
+
+  while (1) {
+  }
+  return 0;
 }
--- openocd/debug.sh
+++ openocd/debug.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 path=$(pwd)
 
-openocd -f ${path}/stlink.cfg -f ${path}/stm32l496x.cfg  -c init -c "reset halt" > $(pwd)/log.txt 
+openocd -f ${path}/stlink-dap.cfg -f ${path}/stm32mp15x.cfg  -c init -c "reset_config srst_only" > $(pwd)/log.txt 
 # 一些配置以及烧录语句,根据情况使用:
 
